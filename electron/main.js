da13261d1273128a63817841392404e3df81daeb
Aishwarya-Ramesh-Shetty <shettyaishu050@gmail.com>
2026-07-05 23:44:45 +0530

    Setting up project structure

---
+++ electron/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1200,
+    minHeight: 700,
+
+    autoHideMenuBar: true,
+
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL("http://localhost:5173");
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, "../frontend/dist/index.html"));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
