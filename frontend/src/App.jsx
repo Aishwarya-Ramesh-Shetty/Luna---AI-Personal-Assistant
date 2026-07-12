4d81284a37d2a065cd89a70f8da8d65a2b8eec56
Aishwarya-Ramesh-Shetty <shettyaishu050@gmail.com>
2026-07-12 21:33:26 +0530

    Working on Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Memory from "./pages/Memory";
+import Files from "./pages/Files";
+import Tasks from "./pages/Tasks";
+import Privacy from "./pages/Privacy";
+import Settings from "./pages/Settings";
 
 function App() {
- 
-
-  return(
-    <div>Hi</div>
-  )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/memory" element={<Memory />} />
+        <Route path="/files" element={<Files />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Memory.jsx
@@ -0,0 +1,5 @@
+function Memory() {
+  return <h1>Memory</h1>;
+}
+
+export default Memory;
