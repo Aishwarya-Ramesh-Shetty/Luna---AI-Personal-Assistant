2ab20de4a2a01bb75c5f32776a1f8c3080c30a71
Aishwarya-Ramesh-Shetty <shettyaishu050@gmail.com>
2026-07-18 23:13:11 +0530

    Working on Layout

---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,21 @@
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
+
+function MainLayout() {
+  return (
+    <div className="flex h-screen bg-slate-950 text-white">
+      <Sidebar />
+
+      <div className="flex flex-1 flex-col overflow-hidden">
+        <Header />
+
+        <main className="flex-1 overflow-auto p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default MainLayout;
